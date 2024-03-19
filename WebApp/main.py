d580cb47c9188736948497b2f2e8f1784fe8274c
Samiimasmoudii <samsoum.tecktonick@gmail.com>
2024-03-19 10:06:09 +0100

    Yemchi First Time

--- WebApp/main.py
+++ WebApp/main.py
@@ -1,10 +1,8 @@
-
-import sys
-#sys.path.append('C:\Users\samso\Documents\Programming\PFA2\WebApp\website')
 from website import create_app
 
-age =2 
 
-#app = create_app()e
-#if __name__ == '__main__':
-#    app.run(debug=True)
+
+app = create_app()
+if __name__ == '__main__' :
+    app.run(debug=False)
+
--- WebApp/website/__init__.py
+++ WebApp/website/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
-import sys
-from os.path import dirname,abspath
-d=dirname(dirname(abspath(__file__)))
-sys.path.append(d)
 
 def create_app():
     app= Flask(__name__)
     app.config['SECRET_KEY'] = 'kfjshflakjhflkj'
+    from website.views import views
+    from website.auth import auth
+    app.register_blueprint(views,url_prefix='/')
+    app.register_blueprint(auth,url_prefix='/')
     return app
-
+    
