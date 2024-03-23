6efdb352f93563120e5970c081d32ad703cf6ee2
Samiimasmoudii <samsoum.tecktonick@gmail.com>
2024-03-23 21:44:28 +0100

    Sign-Up working

--- WebApp/website/auth.py
+++ WebApp/website/auth.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template
 auth = Blueprint('auth',__name__ )
 @auth.route ( '/login' )
 def login():
-    return render_template( 'login.html')
+    return render_template( 'login.html', boolean=True )
 
 
 @auth.route('/logout')
