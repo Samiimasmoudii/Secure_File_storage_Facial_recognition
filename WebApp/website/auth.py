c681e79f5c2870993641f7562e17fe8e68eb14c8
Samiimasmoudii <samsoum.tecktonick@gmail.com>
2024-03-24 00:00:12 +0100

    Signup Flashing works

--- WebApp/website/auth.py
+++ WebApp/website/auth.py
@@ -1,9 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request,flash
 
 auth = Blueprint('auth',__name__ )
-@auth.route ( '/login' )
+@auth.route( '/login' , methods=['POST' ,'GET' ])
 def login():
-    return render_template( 'login.html', boolean=True )
+    data = request.form
+    print(data)
+    
+    return render_template( 'login.html')
+
+
+
+
 
 
 @auth.route('/logout')
@@ -11,6 +18,22 @@ def logout():
     return render_template( 'logout.html')
 
 
-@auth.route('/signup')
+@auth.route('/signup',methods=['POST', 'get'])
 def signup():
+    if request.method == 'POST':
+        email=request.form.get('email')
+        firstName=request.form.get('firstName')
+        password1=request.form.get('password1')
+        password2=request.form.get('password2')
+        if len(email)<4:
+           flash('Email must be greater than 4 character' , category ='error')
+        elif len(firstName)<4:
+           flash('First Name must be greater than 4 character' , category ='error')
+        elif len(password1)<7:
+           flash('Password must be greater than 7 characters' , category ='error')
+        elif password1 != password2: 
+            flash('Password dont match' , category ='error')
+        else:
+            flash('Account Created' , category ='success')
+         
     return render_template( 'sign_up.html')
