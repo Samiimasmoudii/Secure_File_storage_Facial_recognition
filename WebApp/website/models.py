0a8211e4b2e9e469c173fcc48a390fbad6040e6b
Samiimasmoudii <samsoum.tecktonick@gmail.com>
2024-04-08 05:03:51 +0100

    Fail

--- WebApp/website/models.py
+++ WebApp/website/models.py
@@ -10,7 +10,16 @@ class User(db.Model, UserMixin):
     first_name = db.Column(db.String(150))
 class Note(db.Model):
     id=db.Column(db.Integer, primary_key=True)
-    data = db.column(db.String(100000))
+    data = db.Column(db.String(100000))
     date=db.Column(db.DateTime(timezone=True), default=func.now())
     user_id=db.Column(db.Integer, db.ForeignKey('User.id'))
-     
+    
+class File(db.Model):
+    __tablename__ = 'file'  # Use lowercase for the table name
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(255), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)  # Correct casing for the table name
+    user = db.relationship('User', backref=db.backref('files', lazy=True)) # lazy allows the user class to be loaded without needing to load all the files. THe files will be loaded when needed 
+
+    def __repr__(self): # Use for debugging 
+        return f"File(id={self.id}, filename='{self.filename}', user_id={self.user_id})"
